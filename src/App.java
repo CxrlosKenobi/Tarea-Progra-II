cc396e073cc1d995035cf2b7e07c49952f3825b3
SebaSinMas <sebastianbustos90@gmail.com>
2022-09-29 22:26:45 -0300

    Created all clases from UML diagram

--- src/App.java
+++ src/App.java
@@ -1,5 +1,55 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("hello, World!");
+
     }
 }
+
+class OrdenCompra{
+    
+}
+
+class Cliente{
+
+}
+
+class Direccion{
+
+}
+
+class DocTributario{
+
+}
+
+class Boleta{
+
+}
+
+class Factura{
+
+}
+
+class Pago{
+
+}
+
+class Efectivo{
+
+}
+
+class Transferencia{
+
+}
+
+class Tarjeta{
+
+}
+
+class Articulo{
+
+}
+
+class detalleOrden{
+
+}
+
