089120e85dd3d9b88d27fba24209d54d6b84b124
SebaSinMas <sebastianbustos90@gmail.com>
2022-10-07 22:39:08 -0300

    Implemented Cliente and Direccion
    We have developed this classes, with their respective attributes and methods.

--- src/packages/Cliente.java
+++ src/packages/Cliente.java
@@ -2,18 +2,25 @@ package packages;
 public class Cliente {
   private String nombre;
   private String rut;
-  public Cliente(){}
+  
+  public Cliente(String nombre, String rut) {
+    this.nombre = nombre;
+    this.rut = rut;
+  }
 
   //getters y setters
   public void setNombre(String nombre){
     this.nombre = nombre;
   }
+
   public String getNombre(){
     return nombre;
   }
+
   public void setRut(String rut){
     this.rut = rut;
   }
+
   public String getRut(){
     return rut;
   }
--- src/packages/Direccion.java
+++ src/packages/Direccion.java
@@ -1,5 +1,22 @@
 package packages;
 
 public class Direccion {
-    
+    private String direccion;
+
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    //getters y setters
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+    public String getDireccion(){
+        return direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion [direccion=" + direccion + "]";
+    }
 }
